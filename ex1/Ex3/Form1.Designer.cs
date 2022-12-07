8ca0b4b100cd373f962d01a6f2bda37aef90fa00
Alexmarques22 <120022336+Alexmarques22@users.noreply.github.com>
2022-12-07 10:41:48 -0300

    Add files via upload

---
+++ ex1/Ex3/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Ex3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vermelho = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // vermelho
+            // 
+            this.vermelho.AutoSize = true;
+            this.vermelho.BackColor = System.Drawing.Color.White;
+            this.vermelho.Font = new System.Drawing.Font("Algerian", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vermelho.ForeColor = System.Drawing.Color.Red;
+            this.vermelho.Location = new System.Drawing.Point(334, 204);
+            this.vermelho.Name = "vermelho";
+            this.vermelho.Size = new System.Drawing.Size(79, 41);
+            this.vermelho.TabIndex = 0;
+            this.vermelho.Text = "fim";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Algerian", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(221, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(340, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "mudar cor";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.vermelho);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label vermelho;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
